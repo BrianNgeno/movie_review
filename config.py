2d821b973dc8886fd8e88093ece5583ef8fccc8c
bkn.ngeno@gmail.com <bkn.ngeno@gmail.com>
2022-05-12 10:33:25 +0300

    updated the config file

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://brian:admin123@localhost/watch'
+    
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -16,10 +16,14 @@ class Config:
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswitch("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://",1)
+
 
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://brian:admin123@localhost/watch'
     DEBUG = True
 
 config_options = {
